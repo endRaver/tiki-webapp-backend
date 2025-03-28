9939babc86c482c5358c1fd5c35c33746e628c46
Saltypeesh <tungthanh254@gmail.com>
2025-03-28 20:27:03 +0700

    Implement authentication, refresh & access tokens - Add user model with the according field - Create route and functionality related to user. - Add middleware for authentication - Setup refresh token via redis

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json({ limit: '10mb' }));
+app.use(cookieParser());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+
+app.use("/api/auth", authRoutes);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+  connectDB();
+});
