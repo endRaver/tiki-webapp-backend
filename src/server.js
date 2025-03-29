8541f00fd83d5a49cfeb40b9a085edbbbccc46ac
Saltypeesh <tungthanh254@gmail.com>
2025-03-30 01:10:54 +0700

    Update CORS configuration in server.js to use a fixed origin for development

--- src/server.js
+++ src/server.js
@@ -21,9 +21,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.NODE_ENV === "production" 
-    ? "your-production-frontend-url" 
-    : "http://localhost:5173",
+  origin: "http://localhost:5173",
   credentials: true,
 }));
 
