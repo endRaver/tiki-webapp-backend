440bb67841b11a5935af76991c3c3acf88548218
Tung Ngo <tungthanh254@gmail.com>
2025-04-01 15:48:49 +0700

    Update CORS configuration in server.js to support multiple origins for development

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5173" || "https://localhost:5174" || "http://localhost:3000",
   credentials: true,
 }));
 
