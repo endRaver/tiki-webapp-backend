7ae6d081694e4b68632a6e7a81d4330da4851d36
Saltypeesh <tungthanh254@gmail.com>
2025-04-17 01:03:55 +0700

    Fix success_url formatting in createCheckoutSession function
    - Updated the success_url in the createCheckoutSession function to correctly interpolate the CHECKOUT_SESSION_ID variable, ensuring proper URL generation for successful payment sessions.

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -101,7 +101,7 @@ export const createCheckoutSession = async (req, res) => {
       line_items: lineItems,
       mode: 'payment',
       currency: 'vnd',
-      success_url: `${process.env.CLIENT_URL}/purchase-success?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `${process.env.CLIENT_URL}/purchase-success?session_id=${CHECKOUT_SESSION_ID}`,
       cancel_url: `${process.env.CLIENT_URL}/checkout`,
       discounts: stripeCouponId ? [{ coupon: stripeCouponId }] : [],
       metadata: {
