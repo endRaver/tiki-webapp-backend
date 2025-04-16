9fb3db3dbcd0d0b4934fb715515d7289cafa8ed8
Saltypeesh <tungthanh254@gmail.com>
2025-04-17 00:34:08 +0700

    Refactor CORS configuration to use a dynamic origin check
    - Updated the CORS middleware in server.js to implement a dynamic origin check against a predefined list of allowed origins, enhancing security and flexibility for client access.

--- src/server.js
+++ src/server.js
@@ -23,14 +23,19 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://tiki-webapp-frontend.onrender.com'
+];
+
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'http://localhost:3000',
-    'http://localhost:5174',
-    'https://tiki-webapp-frontend.onrender.com/'
-  ],
-  credentials: true,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
 }));
 
 app.use("/api/auth", authRoutes);
